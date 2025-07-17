85e2c1412398eafce9858adab24082d7f05603af
Jesús Danilo castellanos <castellanosjesus801@gmail.com>
2025-07-16 21:59:59 -0500

    #

--- Controllers/DietaController.cs
+++ Controllers/DietaController.cs
@@ -4,7 +4,6 @@ using RunGymFront.Services;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
